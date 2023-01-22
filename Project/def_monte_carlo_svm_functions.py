3f522f973f187dd15bf3fe06f7a74ad84a2ad8d7
VALENTIN JACOUTON <jacouton.valentin@gmail.com>
2023-01-22 15:44:04 +0100

    Monte-Carlo: implement gaussian noise

--- Project/def_monte_carlo_svm_functions.py
+++ Project/def_monte_carlo_svm_functions.py
@@ -1,2 +1,62 @@
+import numpy as np
+import pandas as pd
 
 
+#%% 
+
+def generate_gaussian_noises(N, dim):
+    """
+    Generate a set of N noises on dim dimensions.
+
+    Parameters
+    ----------
+    N : int
+        Number of noises to generate.
+    dim : int
+        Number of dimensions of the gaussian noise.
+
+    Returns
+    -------
+    Numpy array
+        Set of noises.
+
+    """
+    mu=0
+    sigma=0.025
+        
+    return np.random.normal(mu, sigma,[N,dim])
+
+def noise_set(scaled_data, i, noises):
+    """
+    Set the i th noise to a set of scaled data
+
+    Parameters
+    ----------
+    scaled_data : Numpy Array
+        Data to noise.
+    i : int
+        Noise to set.
+    noises : Numpy Array
+        Noise to set.
+
+    Returns
+    -------
+    noised_set : Numpy Array
+        Noised data.
+
+    """
+    noised_set = np.copy(scaled_data)
+    for k in range(len(scaled_data)):
+        noised_set[k,:] = scaled_data[k,:] + noises[i,:]
+        print(noised_set[k,:])
+    return noised_set
+
+#%% test
+
+A = np.array([[1.0,1.0,1.0],
+              [1.0,1.0,1.0],
+              [1.0,1.0,1.0],
+              [1.0,1.0,1.0]])
+noises = generate_gaussian_noises(5, 3)
+
+B = noise_set(A, 2, noises)
---
+++ Project/main_monte_carlo_svm.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from def_monte_carlo_svm_functions import *
+
+
+#%%
