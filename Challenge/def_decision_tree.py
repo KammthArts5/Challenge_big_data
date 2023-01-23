37936df1e13570dba47bdd75f47b581c21eaaf9c
KammthArts5 <jacouton.valentin@gmail.com>
2023-01-23 15:03:21 +0100

    Comment code

--- Challenge/def_decision_tree.py
+++ Challenge/def_decision_tree.py
@@ -6,6 +6,24 @@ from sklearn.metrics import confusion_matrix
 
 #%%
 def dt_model(X,y):
+    """
+    Generate a decision tree model and it extract its labels
+
+    Parameters
+    ----------
+    X : Pandas DataFrame
+        Inputs train data.
+    y : Pandas DataFrame
+        Output train data.
+
+    Returns
+    -------
+    clf : Scikit learn Decision Tree
+        Decision tree classifier model.
+    labels : list
+        labels.
+
+    """
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(X, y)
     labels = [str(i) for i in clf.classes_]
@@ -14,6 +32,25 @@ def dt_model(X,y):
 #%% plot tree
 
 def save_plot_dt(model,feature_name,labels, file_name="model"):
+    """
+    Save the plot tree in a PNG file
+
+    Parameters
+    ----------
+    model : Scikit learn Decision tree
+        Decision tree to plot.
+    feature_name : Pandas Indexes
+        Name of the features of the data.
+    labels : List
+        Labels.
+    file_name : String, optional
+        Name of the PNG file. The default is "model".
+
+    Returns
+    -------
+    None.
+
+    """
     fig = plt.figure(figsize=(32,18))
     
     tree.plot_tree(model,feature_names=feature_name,class_names=labels, filled=True)
@@ -24,6 +61,26 @@ def save_plot_dt(model,feature_name,labels, file_name="model"):
 #%% Confusion matrix
 
 def conf_matrix(model,X_test,y_test):
+    """
+    Assess the accuracy of the Decision tree model.
+
+    Parameters
+    ----------
+    model : Scikit learn Decision Tree
+        Decision tree to evaluate.
+    X_test : Pandas DataFrame
+        Inputs test data.
+    y_test : Pandas DataFrame
+        Output test data.
+
+    Returns
+    -------
+    cm : TYPE
+        DESCRIPTION.
+    acc : TYPE
+        DESCRIPTION.
+
+    """
     acc = model.score(X_test,y_test)
     y_pred = model.predict(X_test)
     cm = pd.DataFrame(data=confusion_matrix(y_test, y_pred),
